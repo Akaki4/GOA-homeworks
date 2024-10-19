cca526f7bdd09f126735f7d6e9e9d11a184fc22f
Akaki4 <akakiwabria48@gmail.com>
2024-10-19 15:45:17 +0400

    commit

---
+++ lvl 01/homework/lession.py
@@ -0,0 +1,5 @@
+name = "Akaki"
+lastname = 'Tsabria'
+age = 16
+height = 175.0
+print("my name is " +" "+ name+ " " + lastname + " "+ "and im " + " " + str(age) + " "+ "years old" + " "+ "and im "+' '+ str(height))
